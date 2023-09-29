c1122863219d2e5d7f968a0d716cd611a2e5fa3d
pongsapat <golf@kos.co.th>
2023-09-29 13:04:04 +0700

    feat: condition modal to confirm before delete

--- app/page.tsx
+++ app/page.tsx
@@ -1,132 +1,160 @@
-"use client"
-import AddTask from "@/components/add-task"
-import ModalForm, { FormData } from "@/components/modal-form"
+"use client";
+import AddTask from "@/components/add-task";
+import ModalForm, { FormData } from "@/components/modal-form";
 import { TaskContentProps } from "@/components/task";
 import TodoList from "@/components/todo-list";
 import useDialog from "@/hooks/use-dialog";
 import { useToast } from "@/hooks/use-toast";
-import { 
-  PayloadCreateTask, 
-  PayloadUpdateTask, 
-  createTask, 
-  deleteTask, 
-  getTodoList, 
-  updateTask 
+import {
+  PayloadCreateTask,
+  PayloadUpdateTask,
+  createTask,
+  deleteTask,
+  getTodoList,
+  updateTask,
 } from "@/services/todo";
 import moment from "moment";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-
   const { toast } = useToast();
 
   const { open, data, setOpen, setData } = useDialog<TaskContentProps>();
 
   // state
   const [list, setList] = useState<TaskContentProps[]>();
+  const [taskId, setTaskId] = useState<string>("");
+  const [typeModal, setTypeModal] = useState<string>("create");
 
-  // function 
-  const onCreateTask = async(values: FormData) => {
+  // function
+  const onCreateTask = async (values: FormData) => {
     const payload: PayloadCreateTask = {
-      title: values.title
+      title: values.title,
+    };
+
+    const result = await createTask(payload);
+
+    if (result.status && result.data) {
+      setList((prev) => {
+        if (!prev) return;
+        return [result.data, ...prev];
+      });
     }
 
-    const result = await createTask(payload)
+    toast({
+      title: result.status ? "Success" : "Error",
+      description: result.message,
+    });
+  };
 
-    if(result.status && result.data){
-      setList(prev => {
-        if(!prev) return;
-        return [result.data, ...prev]
-      })
-    }
-
-    toast({title: result.status ? "Success" : "Error", description: result.message})
-  }
-
-  const onUpdateTask = async(taskId: string, values: FormData, done?: boolean) => {
+  const onUpdateTask = async (
+    taskId: string,
+    values: FormData,
+    done?: boolean
+  ) => {
     let payload: PayloadUpdateTask = {
-      title: values.title
+      title: values.title,
+    };
+
+    if (done !== undefined) {
+      payload = { ...payload, done };
     }
 
-    if(done !== undefined) {
-      payload = {...payload, done}
+    const result = await updateTask(taskId, payload);
+
+    if (result.status && result.data) {
+      setList((prev) => {
+        if (!prev) return;
+        return prev
+          .map((e) => (e.id === taskId ? result.data : e))
+          .sort((a, b) => moment(b.updatedAt).diff(a.updatedAt));
+      });
     }
 
-    const result = await updateTask(taskId, payload)
+    toast({
+      title: result.status ? "Success" : "Error",
+      description: result.message,
+    });
+  };
 
-    if(result.status && result.data){
-      setList(prev => {
-        if(!prev) return;
-        return prev.map(e => e.id === taskId ? result.data : e).sort((a, b) => moment(b.updatedAt).diff(a.updatedAt))
-      })
-    }
-
-    toast({title: result.status ? "Success" : "Error", description: result.message})
-  }
-
-  const onDeleteTask = async(taskId: string) => {
+  const onDeleteTask = async (taskId: string) => {
     const result = await deleteTask(taskId);
 
-    if(result.status && result.data){
-      setList(prev => {
-        if(!prev) return;
-        return prev.filter(e => e.id !== taskId)
-      })
+    if (result.status && result.data) {
+      setList((prev) => {
+        if (!prev) return;
+        return prev.filter((e) => e.id !== taskId);
+      });
     }
-    toast({title: result.status ? "Success" : "Error", description: result.message})
-  }
+    toast({
+      title: result.status ? "Success" : "Error",
+      description: result.message,
+    });
+  };
 
   const onSubmit = (values: FormData) => {
     setOpen(false);
-    if(!data){
-      onCreateTask(values)
-    }else{
-      onUpdateTask(data.id, values)
+    if (!data) {
+      onCreateTask(values);
+    } else {
+      onUpdateTask(data.id, values);
     }
-  }
+  };
 
   const onEditClick = (value: TaskContentProps) => {
+    setTypeModal("edit");
     setOpen(true);
-    setData(value)
-  }
+    setData(value);
+  };
 
   const onDeleteClick = (value: TaskContentProps) => {
-    const taskId = value.id
-    onDeleteTask(taskId)
-  }
+    setTypeModal("delete");
+    setOpen(true);
+    if (value.id) {
+      setTaskId(value.id);
+    }
+  };
+
+  const onClickDeleteConfirm = () => {
+    onDeleteTask(taskId);
+    setOpen(false);
+  };
 
   const onCheckedChange = (checked: boolean, value: TaskContentProps) => {
-    const taskId = value.id
-    const values: FormData =  {
-      title: value.title
-    }
+    const taskId = value.id;
+    const values: FormData = {
+      title: value.title,
+    };
 
-    onUpdateTask(taskId, values, checked)
-  }
+    onUpdateTask(taskId, values, checked);
+  };
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       const result = await getTodoList();
 
-      let list:TaskContentProps[] = [];
-      if(result.status && result.data){
+      let list: TaskContentProps[] = [];
+      if (result.status && result.data) {
         list = result.data;
       }
-      setList(list)
+      setList(list);
     })();
-  },[]) 
+  }, []);
 
   return (
     <main className="max-w-4xl mx-auto min-h-screen flex flex-col items-center py-20">
-      <ModalForm 
-        data={data?.title} 
-        open={open} 
-        onSubmit={onSubmit} 
-        onOpenChange={value => setOpen(value)}
+      <ModalForm
+        typeModal={typeModal}
+        data={data?.title}
+        open={open}
+        onSubmit={onSubmit}
+        onCancel={() => setOpen(false)}
+        onOpenChange={(value) => setOpen(value)}
+        onClickConfirm={onClickDeleteConfirm}
       />
       <h1 className="text-2xl font-bold mb-3 capitalize">todo list app</h1>
       <div className="w-2/3">
-        <AddTask onClick={() => setOpen(true)}/>
+        <AddTask onClick={() => setOpen(true)} />
         <TodoList
           list={list}
           onEditClick={onEditClick}
@@ -135,5 +163,5 @@ export default function Home() {
         />
       </div>
     </main>
-  )
+  );
 }
--- components/modal-form.tsx
+++ components/modal-form.tsx
@@ -1,77 +1,140 @@
 import * as z from "zod";
 import { FC, memo, useEffect } from "react";
 import { DialogProps } from "@radix-ui/react-dialog";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
-import { Input } from "./ui/input"
-import { Button } from "./ui/button"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export type FormData = z.infer<typeof TodoSchema>
+export type FormData = z.infer<typeof TodoSchema>;
 
 const TodoSchema = z.object({
-  title: z.string()
-})
+  title: z.string(),
+});
 
 interface ModalFormProps extends DialogProps {
+  typeModal: string;
   data?: string;
   onSubmit: (values: FormData) => void;
+  onCancel: () => void;
+  onClickConfirm: () => void;
 }
 
-const ModalForm:FC<ModalFormProps> = ({data, onSubmit, ...props}) => {
-
+const ModalForm: FC<ModalFormProps> = ({
+  typeModal,
+  data,
+  onSubmit,
+  onCancel,
+  onClickConfirm,
+  ...props
+}) => {
   const form = useForm<FormData>({
-      resolver: zodResolver(TodoSchema), 
-      defaultValues: {
-        title: ""
-      }
-    });
+    resolver: zodResolver(TodoSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
 
   useEffect(() => {
-    if(!props.open)return;
+    if (!props.open) return;
 
-    form.reset({title: data || ""});
-  },[data, props.open]);
+    form.reset({ title: data || "" });
+  }, [data, props.open]);
 
   return (
     <Dialog {...props}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>{`${!data ? "Add" : "Edit"} task`}</DialogTitle>
+          <DialogTitle>{`${
+            typeModal === "edit"
+              ? "Edit"
+              : typeModal === "delete"
+              ? "Delete"
+              : "Create"
+          } task`}</DialogTitle>
         </DialogHeader>
+        {typeModal === "delete" && (
+          <div className="text-md">Do you want to delete this task?</div>
+        )}
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="flex gap-5 items-center">
-              <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel>What are you thinking?</FormLabel>
-                    <FormControl>
-                      <Input 
-                        className="w-full border border-red-500" 
-                        placeholder="Type here... 😬😱🥶" 
-                        {...field} 
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
+              {typeModal === "create" ||
+                (typeModal === "edit" && (
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem className="w-full">
+                        <FormLabel>What are you thinking?</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="w-full border border-red-500"
+                            placeholder="Type here... 😬😱🥶"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                ))}
+
+              <div
+                className={
+                  typeModal === "delete" ? "flex w-full justify-end" : ""
+                }
+              >
+                {typeModal === "delete" ? (
+                  <>
+                    <Button
+                      className="translate-y-4 mx-3 bg-gray-500 hover:bg-gray-600"
+                      type="button"
+                      onClick={onCancel}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      className={
+                        typeModal === "delete"
+                          ? "translate-y-4 bg-red-500 hover:bg-red-600"
+                          : "translate-y-4"
+                      }
+                      type="button"
+                      onClick={onClickConfirm}
+                    >
+                      Confirm
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Button
+                      className="translate-y-4"
+                      type="submit"
+                      disabled={
+                        !form.formState.isValid || !form.formState.isDirty
+                      }
+                    >
+                      Submit
+                    </Button>
+                  </>
                 )}
-              />
-              <Button 
-                className="translate-y-4" 
-                type="submit" 
-                disabled={!form.formState.isValid || !form.formState.isDirty}>
-                  Submit
-            </Button>
+              </div>
             </div>
           </form>
         </Form>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
 export default memo(ModalForm);
